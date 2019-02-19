1dd82dedd7dafa338dd490d1133289a25566e715
Antony Berezowsky <krauzes@outlook.com>
2019-02-19 12:15:48 +0200

    wabadadab

--- Rozmetka/Tables/Status.cs
+++ Rozmetka/Tables/Status.cs
@@ -11,6 +11,6 @@ namespace Rozmetka.Tables
         public int Id { get; set; }
         public string Name { get; set; }
         public virtual ICollection<User> Users { get; set; }       
-        33
+        
     }
 }
